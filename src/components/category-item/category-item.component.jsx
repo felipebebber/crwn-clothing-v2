8d6c946b4db5a5abdd2456d43a91f2e10b7a2b7f
Felipe Bebber <fbebber1@gmail.com>
2023-09-11 00:17:17 -0300

    Criação de componente directory + category

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,17 @@
+import './category-item.styles.scss';
+
+const CategoryItem = ({ category }) => {
+    const { title, id, imageUrl } = category;
+    
+    return (
+        <div className="category-container">
+            <div style={{backgroundImage: `url(${imageUrl})`}} className="background-image"></div>
+            <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+            </div>
+        </div>
+    )
+}
+
+export default CategoryItem;
